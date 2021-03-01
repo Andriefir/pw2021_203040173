4cc548e0296cffd34319cd5c1f46b930636457ca
Andriefir <andriefirmansyah334@gmail.com>
2021-03-01 19:44:09 +0700

    Menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Andrie Firmansyah
+203040173
+https://github.com/Andriefir/pw2021_203040173
+Pertemuan 3 - 19 Februari 2021
+Mempelajari tentang Pengulangan dan Pengkondisian dalam PHP
+*/ 
+?>
+
+<?php
+// Pengulangan
+/* 
+for
+while
+do.. while
+foreach : pengulangan khusus array
+*/
+
+// for( $i = 0; $i < 5; $i++ ) {
+//     echo "Hello World <br>";
+// }
+
+// $i = 0;
+// while( $i < 5 ) {
+//     echo "Hello World <br>";
+// $i++;
+// }
+
+// $i = 10;
+// do {
+//     echo "Hello World <br>";
+// $i++;
+// } while( $i < 5 )
+?>
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Andrie Firmansyah
+203040173
+https://github.com/Andriefir/pw2021_203040173
+Pertemuan 3 - 19 Februari 2021
+Mempelajari tentang Pengulangan dan Pengkondisian dalam PHP
+*/
+?>
+
+<?php 
+// Pengkondisian / Percabangan
+/*
+if else
+if else if else
+ternary
+switch
+*/
+
+
+$x = 2;
+if( $x < 20 ) {
+    echo "Benar";
+} else if( $x == 20 ) {
+    echo "Bingo!"; 
+} else {
+    echo "Salah";
+}
+
+?>
