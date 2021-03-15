60f6cc9daf13ebd86c7311d6261ce2449e137c38
Andriefir <andriefirmansyah334@gmail.com>
2021-03-15 11:02:26 +0700

    Menyelesaikan video pertemuan 7

---
+++ pertemuan7/latihan1.php
@@ -0,0 +1,53 @@
+<?php
+/* 
+    Andrie Firmansyah
+    203040173
+    https://github.com/Andriefir/pw2021_203040173
+    Pertemuan 7 - 26 Februari 2021 
+    Materi Minggu ini mempelajari mengenai GET & POST
+*/
+?>
+
+<?php 
+// Superglobals 
+// Variable global milik php
+// Termasuk array associative
+// $_GET 
+// bisa di tambahkan melalui URL
+// jika lebih dari 1 variable tambah &
+// var_dump($_GET);
+$mahasiswa = [
+    [
+        "nama" => "Mochammad Andrie Firmansyah", 
+        "nrp" => "203040173",
+        "email" => "andriefirmansyah334@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "Andrie.png"
+    ]
+];
+
+
+// $_POST
+// $_REQUEST
+// $_SESSION
+// $_COOKIE
+// $_SERVER .....
+// $_ENV
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GET</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <?php foreach ( $mahasiswa as $mhs ) : ?>
+        <ul>
+            <li>
+                <a href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nrp=<?=$mhs["nrp"]; ?>&email=<?= $mhs["email"]; ?>&jurusan=<?= $mhs["jurusan"]; ?>&gambar=<?= $mhs["gambar"]; ?>"><?= $mhs["nama"]; ?></a>
+            </li>
+        </ul>
+    <?php endforeach; ?>
+</body>
+</html>
---
+++ pertemuan7/latihan4.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+    Andrie Firmansyah
+    203040173
+    https://github.com/Andriefir/pw2021_203040173
+    Pertemuan 7 - 26 Februari 2021 
+    Materi Minggu ini mempelajari mengenai GET & POST
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Selamat Datang, <?= $_POST["nama"] ?>!</h1>
+</body>
+</html>
