636a0132c09669b43cf95c4108bbc4a0c8d5ca0d
Andriefir <andriefirmansyah334@gmail.com>
2021-04-18 15:16:59 +0700

    Menyelesaikan pertemuan 10

---
+++ detail.php
@@ -0,0 +1,31 @@
+<?php  
+require 'functions.php';
+
+// Ambil id dari URL
+$id = $_GET['id'];
+
+// Query mahasiswa berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Detail Mahasiswa</title>
+</head>
+<body>
+	
+	<h3>Detail Mahasiswa</h3>
+
+	<ul>
+		<li><img src="img/<?= $mhs['Gambar']; ?>"></li>
+		<li>NRP : <?= $mhs['NRP']; ?></li>
+		<li>Nama : <?= $mhs['Nama']; ?></li>
+		<li>Email : <?= $mhs['Email']; ?></li>
+		<li>Jurusan : <?= $mhs['Jurusan']; ?></li>
+		<li><a href="">ubah</a> | <a href="">hapus</a></li>
+		<li><a href="latihan3.php">Kembali ke daftar mahasiswa</a></li>
+	</ul>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php  
+
+function koneksi()
+{
+	return mysqli_connect('localhost', 'root', '', 'pw_203040173');
+}
+
+function query($query)
+{
+	$conn = koneksi();
+
+	$result = mysqli_query($conn, $query);
+
+	// Jika hasilnya hanya 1 data
+	if(mysqli_num_rows($result) == 1){
+		return mysqli_fetch_assoc($result);
+	}
+
+	$rows = [];
+	while ($row = mysqli_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+
+	return $rows;
+}
+
+function tambah($data)
+{
+	$conn = koneksi();
+
+	$Gambar = htmlspecialchars($data['Gambar']);
+	$Nama = htmlspecialchars($data['Nama']);
+	$NRP = htmlspecialchars($data['nrp']);
+	$Email = htmlspecialchars($data['Email']);
+	$Jurusan = htmlspecialchars($data['Jurusan']);
+
+	$query = "INSERT INTO
+	            mahasiswa
+	            VALUES
+	          (null, '$Gambar', '$Nama', '$NRP', '$Email', '$Jurusan');
+	          ";
+	mysqli_query($conn, $query);
+	
+	return mysqli_affected_rows($conn);
+}
+?>
